8c714b6e29ddfd4b4f376f4febbc1a39bcdbd1b8
hcxra <hannahccera@gmail.com>
2024-12-18 22:33:55 -0500

    updated url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api/tasks';
+const API_URL = 'https://hw5todo-app.onrender.com'
 
 // Get all tasks
 export const getTasks = async () => {
